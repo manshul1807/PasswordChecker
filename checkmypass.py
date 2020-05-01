e4e3fd8ebb125d4bfc7be62d6057ec4adf4b400c
manshul1807 <arora.manshul@gmail.com>
2020-05-01 16:24:41 +0200

    Final Commit

--- checkmypass.py
+++ checkmypass.py
@@ -1,5 +1,8 @@
-import requests  # to request
+import requests
 import hashlib
+import sys
+
+# Writing function to request data from website
 
 
 def request_api_data(query_char):
@@ -11,9 +14,31 @@ def request_api_data(query_char):
     return res
 
 
+def get_password_leaks_count(hashes, hash_to_check):
+    hashes = (line.split(':') for line in hashes.text.splitlines())
+    for h, count in hashes:
+        if h == hash_to_check:
+            return count
+    return 0
+
+
 def pwned_api_check(password):
     sha1password = hashlib.sha1(password.encode('utf-8')).hexdigest().upper()
-    return sha1password
+    first5_char, tail = sha1password[:5], sha1password[5:]
+    response = request_api_data(first5_char)
+    return get_password_leaks_count(response, tail)
 
 
-pwned_api_check('123')
+def main(args):
+    for password in args:
+        count = pwned_api_check(password)
+        if count:
+            print(
+                f'{password} was found {count} times ..You should change your password.')
+        else:
+            print(f'{password} was not found. Enjoy!')
+    return 'done!'
+
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv[1:]))
